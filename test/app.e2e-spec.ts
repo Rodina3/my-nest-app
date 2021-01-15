8abbef1d0b3ef96f7bae4956ff03e75407bfe274
RongZhou <rzhou@thoughtworks.com>
2021-01-15 19:58:11 +0800

    feat: add e2e tests for happy path

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,66 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET /', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('GET /cats', () => {
+    return request(app.getHttpServer())
+      .get('/cats')
+      .expect(200)
+      .expect('This action retrieves all cats.');
+  });
+
+  it('GET /cats/:id', async (done) => {
+    const id = 123;
+    const response = await request(app.getHttpServer()).get(`/cats/${id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(`This action retrieve cat with id ${id}`);
+    done();
+  });
+
+  it('DELETE /cats/:id', async (done) => {
+    const id = 123;
+    const response = await request(app.getHttpServer()).delete(`/cats/${id}`);
+
+    expect(response.status).toBe(501);
+    expect(response.body.message).toBe(`Not implement yet`);
+    done();
+  });
+
+  describe('POST /cats', () => {
+    it('should create successfully given valid parameters', async (done) => {
+      const name = 'Amy';
+      const age = 3;
+      const color = 'white';
+
+      const response = await request(app.getHttpServer())
+        .post('/cats')
+        .send({ name: 'Amy', age: 3, color: 'white' });
+
+      expect(response.status).toBe(201);
+      expect(response.text).toBe(
+        `This action adds cat with name: ${name}, age: ${age} and color: ${color}`,
+      );
+      done();
+    });
+
+    // it('should create failed given invalid parameters', async (done) => {
+    //   const response = await request(app.getHttpServer())
+    //     .post('/cats')
+    //     .send({ name: '', age: 100, color: 123 });
+    //
+    //   console.log(response);
+    //   expect(response.status).toBe(400);
+    //
+    //   done();
+    // });
+  });
+
+  afterAll(async () => await app.close());
 });
