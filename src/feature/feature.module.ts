9aeec3741cc809958f6f5c2329ad093589c6459e
RongZhou <rzhou@thoughtworks.com>
2021-01-15 15:10:16 +0800

    feat: add cat controller and service

--- src/feature/feature.module.ts
+++ src/feature/feature.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CatController } from './cat/cat.controller';
+import { CatService } from './cat/cat.service';
 
-@Module({})
+@Module({
+  controllers: [CatController],
+  providers: [CatService],
+})
 export class FeatureModule {}
