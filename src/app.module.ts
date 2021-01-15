1103d1527709b0ebe88cdebc216398c5ad11ec00
RongZhou <rzhou@thoughtworks.com>
2021-01-15 14:58:27 +0800

    feat: remove controller folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatController } from './cat/controller/cat/cat.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
