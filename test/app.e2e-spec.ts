7781956b0dd6c5474cd98e9803a7991905ff1ddd
RongZhou <rzhou@thoughtworks.com>
2021-01-27 15:22:57 +0800

    refactor: remove useless code

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,13 +15,6 @@
 //     await app.init();
 //   });
 //
-//   it('GET /', () => {
-//     return request(app.getHttpServer())
-//       .get('/')
-//       .expect(200)
-//       .expect('Hello World!');
-//   });
-//
 //   it('GET /cats', () => {
 //     return request(app.getHttpServer())
 //       .get('/cats')
