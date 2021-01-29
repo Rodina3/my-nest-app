b74df0fa1913a36d1ce4672e049a6214055c0b97
RongZhou <rzhou@thoughtworks.com>
2021-01-29 10:19:29 +0800

    refactor: rename misleading variable name in cat.service.spec.ts

--- src/cat/service/cat.service.spec.ts
+++ src/cat/service/cat.service.spec.ts
@@ -4,21 +4,21 @@ import { Repository } from 'typeorm';
 import { NotFoundException } from '@nestjs/common';
 
 describe('CatServiceTest', () => {
-  let mockRepo: Repository<CatEntity>;
+  let repo: Repository<CatEntity>;
   let service: CatService;
 
   beforeEach(() => {
-    mockRepo = new Repository<CatEntity>();
-    service = new CatService(mockRepo);
+    repo = new Repository<CatEntity>();
+    service = new CatService(repo);
   });
 
   it('should retrieve all cats when retrieveCats() called', async () => {
     const catEntities = [buildCatEntity(1), buildCatEntity(2)];
-    mockRepo.find = jest.fn(() => Promise.resolve(catEntities));
+    repo.find = jest.fn(() => Promise.resolve(catEntities));
 
     const result = await service.retrieveCats();
 
-    expect(mockRepo.find).toHaveBeenCalledTimes(1);
+    expect(repo.find).toHaveBeenCalledTimes(1);
     expect(result.length).toBe(2);
   });
 
@@ -26,18 +26,18 @@ describe('CatServiceTest', () => {
     it('should retrieve cat by id given id exist in repo', async () => {
       const id = 1;
       const catEntity = buildCatEntity(id);
-      mockRepo.findOne = jest.fn(() => Promise.resolve(catEntity));
+      repo.findOne = jest.fn(() => Promise.resolve(catEntity));
 
       const result = await service.retrieveCatById(id);
 
-      expect(mockRepo.findOne).toHaveBeenCalledTimes(1);
-      expect(mockRepo.findOne).toHaveBeenCalledWith(id);
+      expect(repo.findOne).toHaveBeenCalledTimes(1);
+      expect(repo.findOne).toHaveBeenCalledWith(id);
       expect(result).toEqual(catEntity);
     });
 
     it('should throw NotFoundException given id not exist in repo', async () => {
       const id = 999;
-      mockRepo.findOne = jest.fn(() => Promise.resolve(undefined));
+      repo.findOne = jest.fn(() => Promise.resolve(undefined));
 
       expect.assertions(2);
       try {
@@ -51,12 +51,13 @@ describe('CatServiceTest', () => {
 
   it('should return added catEntity when addCat() called given catEntity', async () => {
     const catEntity = buildCatEntity(1);
-    mockRepo.save = jest.fn((): any => Promise.resolve(catEntity));
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    repo.save = jest.fn((): any => Promise.resolve(catEntity));
 
     const result = await service.addCat(catEntity);
 
-    expect(mockRepo.save).toHaveBeenCalledTimes(1);
-    expect(mockRepo.save).toHaveBeenCalledWith(catEntity);
+    expect(repo.save).toHaveBeenCalledTimes(1);
+    expect(repo.save).toHaveBeenCalledWith(catEntity);
     expect(result).toEqual(catEntity);
   });
 });
